e7cbafa14dd8b397009b58c09b667228d7cdb26c
gardncl <gardncl@gmail.com>
2017-05-12 21:41:07 -0400

    Gradle project created for java backend

---
+++ src/main/java/gardncl/whobrokeitlast/WhobrokeitlastApplication.java
@@ -0,0 +1,12 @@
+package gardncl.whobrokeitlast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhobrokeitlastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhobrokeitlastApplication.class, args);
+	}
+}
