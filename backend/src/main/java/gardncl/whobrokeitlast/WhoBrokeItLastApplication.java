ca836d957c249272d89cf6310407880a8da6e972
gardncl <gardncl@gmail.com>
2017-08-23 23:00:54 -0400

    lambda now works so fixed code

--- backend/src/main/java/gardncl/whobrokeitlast/WhoBrokeItLastApplication.java
+++ backend/src/main/java/gardncl/whobrokeitlast/WhoBrokeItLastApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WhoBrokeItLastApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WhoBrokeItLastApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WhoBrokeItLastApplication.class, args);
+    }
+}
