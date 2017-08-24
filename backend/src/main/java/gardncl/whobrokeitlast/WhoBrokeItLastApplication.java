2088efec913dcdbd5821728dc9d2532bebf491ec
gardncl <gardncl@gmail.com>
2017-08-23 22:49:44 -0400

    yet another broken build test

--- backend/src/main/java/gardncl/whobrokeitlast/WhoBrokeItLastApplication.java
+++ backend/src/main/java/gardncl/whobrokeitlast/WhoBrokeItLastApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WhoBrokeItLastApplication
+public class WhoBrokeItLastApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WhoBrokeItLastApplication.class, args);
 	}
-}
+
